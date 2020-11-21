8da7591bc590ac9d8393a703f3769e1079d87d0b
Yimura <andreas.maerten@scarlet.be>
2020-11-21 18:10:34 +0100

    App: Added OnAppLinkRequestReceived handler to parse token URL's from android

--- EindProject/App.cs
+++ EindProject/App.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly:XamlCompilation(XamlCompilationOptions.Compile)]
@@ -6,14 +9,27 @@ namespace EindProject
 {
 	public class App : Application
 	{
+		public static string Token;
+
 		public App ()
 		{
-			MainPage = new EindProject.MainPage ();
+			MainPage = new MainPage();
 		}
 
-		protected override void OnStart ()
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+			var token = uri.Segments[2];
+
+			Preferences.Set("token", token);
+
+			App.Token = token;
+
+			Current.MainPage.DisplayAlert("Successful Login", token, "Ok");
+        }
+
+        protected override void OnStart ()
 		{
-			// Handle when your app starts
+			App.Token = Preferences.Get("token", "");
 		}
 
 		protected override void OnSleep ()
