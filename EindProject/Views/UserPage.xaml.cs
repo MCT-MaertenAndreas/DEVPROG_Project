8a69d142281876cbde782f4394bc0280884d8b18
Yimura <andreas.maerten@scarlet.be>
2020-11-21 18:09:52 +0100

    Views: Small changes to UserPage (unfinished)

--- EindProject/Views/UserPage.xaml.cs
+++ EindProject/Views/UserPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using EindProject.Models;
 using EindProject.Repositories;
+using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,16 +10,33 @@ namespace EindProject
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UserPage : ContentPage
     {
+        private Leader leader;
+        private Stats stats;
+
         public UserPage()
         {
             InitializeComponent();
         }
 
-        private async void LoadUserData(string uuid = "3f8ff476-eda5-4d56-93bf-7d04c10400b8")
+        public UserPage(Leader leader)
         {
-            Stats stats = await WakaTimeRepo.GetUserStats(uuid);
+            this.leader = leader;
 
+            InitializeComponent();
 
+            this.LoadContent();
+        }
+
+        private async void LoadContent()
+        {
+            imgUser.Source = this.leader.User.Photo;
+
+            this.stats = await WakaTimeRepo.GetUserStats(this.leader.User.Id);
+        }
+
+        private async Task<Stats> LoadUserData(string uuid = "3f8ff476-eda5-4d56-93bf-7d04c10400b8")
+        {
+            return await WakaTimeRepo.GetUserStats(uuid);
         }
     }
 }
