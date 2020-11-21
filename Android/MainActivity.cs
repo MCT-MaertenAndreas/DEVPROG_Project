39cc7e36de41b5c47f81b18708a26ed45bd3e3a1
Yimura <andreas.maerten@scarlet.be>
2020-11-21 18:06:16 +0100

    Android: Added URL scheme that should allow for redirecting from browser to application

--- Android/MainActivity.cs
+++ Android/MainActivity.cs
@@ -11,6 +11,18 @@ using Android.OS;
 namespace EindProject.Droid
 {
 	[Activity (Label = "EindProject.Droid", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[IntentFilter(
+		new [] { Android.Content.Intent.ActionView },
+		DataScheme = "https",
+		DataHost = "wakatime.damon.sh",
+		DataPathPrefix = "/android",
+		AutoVerify = true,
+		Categories = new []
+        {
+			Android.Content.Intent.CategoryDefault,
+			Android.Content.Intent.CategoryBrowsable
+        }
+	)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
