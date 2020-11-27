bbc7f0810414a703d6651262aec199c8bde08dfd
Yimura <andreas.maerten@scarlet.be>
2020-11-27 16:46:49 +0100

    App: Added memory caching to speed up page loading times

---
+++ EindProject/Models/CacheProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EindProject.Models
+{
+    public class CacheProvider
+    {
+        List<string> keys = new List<string>();
+        private IMemoryCache cache;
+
+        public CacheProvider()
+        {
+            this.cache = new MemoryCache(new MemoryCacheOptions { });
+        }
+
+        public T Get<T>(string key)
+        {
+            if (this.cache.TryGetValue(key, out T value))
+                return value;
+            else
+                return default(T);
+        }
+
+        internal void Remove(string key)
+        {
+            this.keys.Remove(key);
+            this.cache.Remove(key);
+        }
+
+        internal void RemoveAll()
+        {
+            foreach (string item in this.keys)
+            {
+                this.cache.Remove(item);
+            }
+            this.keys.Clear();
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            DateTimeOffset expiry = DateTimeOffset.Now.AddHours(12);
+
+            this.keys.Add(key);
+            this.cache.Set(key, value, expiry);
+        }
+    }
+}
