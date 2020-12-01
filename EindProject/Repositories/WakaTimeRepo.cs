bcb88be1a435689ff19858198375b39ab6de3d40
Yimura <andreas.maerten@scarlet.be>
2020-12-01 22:10:45 +0100

    Repo: WakaTime, optimised get request (try/catch)

--- EindProject/Repositories/WakaTimeRepo.cs
+++ EindProject/Repositories/WakaTimeRepo.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace EindProject.Repositories
 {
     public class WakaTimeRepo
     {
-        private static string sBaseURL = "https://wakatime.com/";
+        private static string BaseURL = "https://wakatime.com/";
 
         public static bool IsTokenValid(string token)
         {
@@ -23,18 +24,11 @@ namespace EindProject.Repositories
         {
             const string sPath = "/api/v1/leaders";
 
-            Leaders leaders;
+            string data = await WakaTimeRepo.Get(sPath + "?page="+ page);
 
-            using (var client = WakaTimeRepo.GetHttpClient())
-            {
-                string data = await client.GetStringAsync(WakaTimeRepo.sBaseURL + sPath + "?page="+ page);
+            if (data == null) return null;
 
-                if (data == null) return null;
-
-                leaders = JsonConvert.DeserializeObject<Leaders>(data);
-            }
-
-            return leaders;
+            return JsonConvert.DeserializeObject<Leaders>(data);
         }
 
         public static async Task<User> GetCurrentUser()
@@ -44,47 +38,45 @@ namespace EindProject.Repositories
 
             const string sPath = "/api/v1/users/current";
 
-            User user;
-            using (var client = WakaTimeRepo.GetHttpClient())
-            {
-                string data;
-                try
-                {
-                    data = await client.GetStringAsync(WakaTimeRepo.sBaseURL + sPath);
-                }
-                catch (Exception)
-                {
-                    return null;
-                }
+            string data = await WakaTimeRepo.Get(sPath);
 
-                if (data == null) return null;
+            if (data == null) return null;
 
-                JObject obj = JsonConvert.DeserializeObject<JObject>(data);
+            JObject obj = JsonConvert.DeserializeObject<JObject>(data);
 
-                if (obj["data"] == null) return null;
+            if (obj["data"] == null) return null;
 
-                user = obj["data"].ToObject<User>();
-            }
-
-            return user;
+            return obj["data"].ToObject<User>();
         }
 
         public static async Task<Stats> GetUserStats(string guid)
         {
             string sPath = $"/api/v1/users/{guid}/stats";
+            
+            string data = await WakaTimeRepo.Get(sPath);
 
-            Stats stats;
+            if (data == null) return null;
 
+            JObject obj = JsonConvert.DeserializeObject<JObject>(data);
+
+            if (obj["data"] == null) return null;
+
+            return obj["data"].ToObject<Stats>();
+        }
+
+        private async static Task<string> Get(string path)
+        {
             using (var client = WakaTimeRepo.GetHttpClient())
             {
-                string data = await client.GetStringAsync(WakaTimeRepo.sBaseURL + sPath);
-
-                if (data == null) return null;
-
-                stats = JsonConvert.DeserializeObject<Stats>(data);
+                try
+                {
+                    return await client.GetStringAsync(WakaTimeRepo.BaseURL + path);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
             }
-
-            return stats;
         }
 
         private static HttpClient GetHttpClient()
