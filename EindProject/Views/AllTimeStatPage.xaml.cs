7da4131c2d4c448b8509921693de7e43a37e7273
Yimura <andreas.maerten@scarlet.be>
2020-12-13 10:33:52 +0100

    feat(Views): Added AllTimeStatPage and NavMenu image

---
+++ EindProject/Views/AllTimeStatPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using EindProject.Models;
+using EindProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EindProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllTimeStatPage : ContentPage
+    {
+        public AllTimeStatPage()
+        {
+            InitializeComponent();
+
+            this.LoadContent();
+        }
+
+        private async void LoadContent()
+        {
+            AllTimeStats stats = await WakaTimeRepo.GetCurrentUserAllTimeStats();
+
+            this.layoutStatsCalculating.IsVisible = !stats.UpToDate;
+            this.layoutStats.IsVisible = stats.UpToDate;
+
+            this.grid.BindingContext = stats;
+        }
+    }
+}
